708662c668f4d13eae8d765072ab7b0db87520ec
Ethan <Ethan@10.39.9.244>
2025-09-18 10:04:59 +0100

    Updating Commit

--- OOSD/Lab1/ie/carlowIT/lab1/ThermTest.java
+++ OOSD/Lab1/ie/carlowIT/lab1/ThermTest.java
@@ -11,12 +11,13 @@ public class ThermTest
 	{ // being main method
 
 		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
-		Thermometer thermB = new Thermometer(10.0);
+		Thermometer thermB = new Thermometer(10.0);	// Create second instance using Constructor #2
+		double tempB = thermB.getCelsius();
 		
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
 		thermA.setCelsius(20.0);
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
-		System.out.println("Temp. of Thermometer B is " + thermB.getCelsius() );
+		System.out.println("Temp. of Thermometer B is " + tempB );
 		
 	} // end main
 } // end class ThermTest
