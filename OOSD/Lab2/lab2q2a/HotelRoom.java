13206a9561513e7d9ad3490efcc16d0c0e395226
Ethan <Ethan@10.39.9.244>
2025-09-18 10:35:27 +0100

    Updated change
    Change-Id: Iab2c50d193fd31d6efce0efe8260a3d78aa52ebd

---
+++ OOSD/Lab2/lab2q2a/HotelRoom.java
@@ -0,0 +1,40 @@
+package lab2q2a;
+
+public class HotelRoom 
+{
+	private int roomNumber;
+	private String roomType;
+	
+	public HotelRoom() 
+	{
+		setRoomNumber(0);
+		setRoomType("Single");
+	}
+	
+	public HotelRoom(int number, String type) 
+	{
+		setRoomNumber(number);
+		setRoomType(type);
+	}
+	
+	public void setRoomNumber(int number)
+	{
+		roomNumber = number;		
+	}
+	
+	public void setRoomType(String type)
+	{
+		roomType = type;		
+	}
+
+
+	public int getRoomNumber()
+	{
+		return roomNumber;
+	}
+	
+	public String getRoomType()
+	{
+		return roomType;
+	}
+}
---
+++ OOSD/Lab2/lab2q2a/HotelRoomDriver.java
@@ -0,0 +1,10 @@
+package lab2q2a;
+
+public class HotelRoomDriver {
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
