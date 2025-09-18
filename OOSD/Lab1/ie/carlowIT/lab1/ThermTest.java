c07da94488d9de26d76b4e2615e3f63d546efac5
Ethan <Ethan@10.39.9.244>
2025-09-18 09:54:13 +0100

    updated commit

---
+++ OOSD/Lab1/ie/carlowIT/lab1/ThermTest.java
@@ -0,0 +1,22 @@
+package ie.carlowIT.lab1;
+
+//Student Name: 		Ethan Payne
+//Student Id Number: 	C00309151
+//Date: 				18/09/25
+//Purpose : 			My first class implementation
+
+public class ThermTest
+{ // begin class ThermTest
+	public static void main(String args[]) 
+	{ // being main method
+
+		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
+		Thermometer thermB = new Thermometer(10.0);
+		
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		thermA.setCelsius(20.0);
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		System.out.println("Temp. of Thermometer B is " + thermB.getCelsius() );
+		
+	} // end main
+} // end class ThermTest
